f0d1506a72865a21f081e530541d831aa228256c
GhostedGaming <epicrobloxgamer41@gmail.com>
2025-05-02 12:10:41 +0000

    Im at school rn i will do more work later

--- run.sh
+++ run.sh
@@ -22,23 +22,23 @@ else
 fi
 
 # Create a FAT32 virtual disk if it doesn't exist
-#if [ ! -f "$FAT32_DISK" ]; then
-#    echo "Creating new FAT32 virtual disk..."
+if [ ! -f "$FAT32_DISK" ]; then
+    echo "Creating new FAT32 virtual disk..."
     
     # Create an empty disk image (100MB)
-#    qemu-img create -f raw "$FAT32_DISK" 100M
+    qemu-img create -f raw "$FAT32_DISK" 100M
     
     # Format the disk with FAT32
     # On Linux:
-#    mkfs.fat -F 32 "$FAT32_DISK"
-#    
-#    echo "FAT32 virtual disk created at $FAT32_DISK"
-#fi
+    mkfs.fat -F 32 "$FAT32_DISK"
+    
+    echo "FAT32 virtual disk created at $FAT32_DISK"
+fi
 
 # Run QEMU with both the bootable image and the FAT32 disk
 # Using more compatible settings
-#qemu-system-x86_64 -rtc base=localtime \
-#    -drive format=raw,file="$DESTINATION_DIR/bootimage-eclipse_os.bin" \
-#    -drive format=raw,file="$FAT32_DISK",media=disk \
-#    -vga std \
-#    -m 128M
+qemu-system-x86_64 -rtc base=localtime \
+    -drive format=raw,file="$DESTINATION_DIR/bootimage-eclipse_os.bin" \
+    -drive format=raw,file="$FAT32_DISK",media=disk \
+    -vga std \
+    -m 128M
